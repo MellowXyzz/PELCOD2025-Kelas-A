63e56566cead4c2ba93cc74ca778c804327bd510
Elo Presil Berfa <elopresil026@gmail.com>
2025-09-13 18:29:38 +0700

    hasil praktek pelcod pertama

---
+++ 250441100008_EloPresilBerfa/pelcod1.py
@@ -0,0 +1,40 @@
+print("="*50)
+print("nama saya elo")
+print("="*50)
+
+# nama = "Elo"
+# print(nama)
+# print(type(nama))
+# boolean = True
+# print(type(boolean))
+
+# penjumlahan = 5+7
+# print("hasilnya adalah",penjumlahan)
+
+# nama_asprak= "andika"
+# nama_calon=nama_asprak
+# print(nama_calon)
+
+
+# #tipe data number
+
+# # tahun_lahir= int(input("masukkan Tahun Lahir :"))
+# # tahun_sekarang= int(input("masukkan Tahun Sekarang :"))
+# # hitung=tahun_sekarang-tahun_lahir
+# # print(hitung)
+
+# bilangan= 51
+# bilangan2= 7
+# # jumlah = int(bilangan)+int(bilangan2)
+# # print(jumlah)
+
+ # cek=bilangan<bilangan2
+ # print(cek)
+# modulus = bilangan % bilangan2
+# print(modulus)
+
+saldo = float(input("Masukkan Saldo Awal:"))
+deposit = float(input("Masukkan Saldo Deposit:"))
+saldo += deposit
+print("Saldo anda adalah",saldo)
+
